f89053fbf2bfdad5f5ecdd307a174d685fc69ec7
fmostakim <fokirmostakim10@gmail.com>
2025-08-12 23:22:59 +0600

    Auto commit on Tue 08/12/2025 at 23:22:59.28

---
+++ DebugChallenge.java
@@ -0,0 +1,14 @@
+public class DebugChallenge {
+    public static void main(String[] args) {
+        String firstPlace = "Rahim";
+        int finalMark = 100;
+
+        double averageScore = 92.75;
+        int finalScore;
+
+        finalScore = (int)averageScore;
+
+        System.out.println("Final score is: "+finalScore);
+
+    }
+}
